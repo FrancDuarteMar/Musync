6660bd53ca6e530531da7abc7ca53adb0ffe0802
Francisco Duarte M <frduartemartinez@davidson.edu>
2023-04-11 01:56:23 -0400

    Added spotify album fetch features

--- musync.js
+++ musync.js
@@ -5,6 +5,11 @@ const {
     credentials
 } = require("./config")
 const passport = require('passport')
+if (typeof localStorage === "undefined" || localStorage === null) {
+    var LocalStorage = require('node-localstorage').LocalStorage;
+    localStorage = new LocalStorage('./scratch');
+}
+let playlistSpotify = []
 const {
     engine: expressHandlebars
 } = require("express-handlebars")
@@ -19,6 +24,14 @@ app.disable("x-powered-by")
 
 var authCallbackPath = '/auth/spotify/callback';
 
+function printArr(arr) {
+    let str = "";
+    for (let item of arr) {
+        if (Array.isArray(item)) str += printArr(item);
+        else str += item + ", ";
+    }
+    return str;
+}
 app.use(expressSession({
     resave: true,
     saveUninitialized: true,
@@ -54,29 +67,27 @@ passport.use(
         },
         function (accessToken, refreshToken, expires_in, profile, done) {
             localStorage.setItem("spotify", {
-                "accessToken":accessToken,
+                "accessToken": accessToken,
                 "refreshToken": refreshToken,
-                "expires":expires_in,
+                "expires": expires_in,
                 "profile": profile
-    
-            });
 
-            console.log("Access Token: "+accessToken)
-            console.log("Refresh Token: "+refreshToken)
-            console.log("Profile: "+ profile)
+            });
+            localStorage.setItem("sAccessToken", accessToken)
+            localStorage.setItem("sRefreshToken", refreshToken)
+            localStorage.setItem("sExpires", expires_in)
+            localStorage.setItem("sProfile", JSON.stringify(profile))
+
+            // console.log("local Storage access token: "+ localStorage.getItem("sAccessToken"))
+            // console.log("Access Token: " + accessToken)
+            // console.log("Refresh Token: " + refreshToken)
+            // console.log("Profile: " + profile)
 
             process.nextTick(function () {
-                // To keep the example simple, the user's spotify profile is returned to
-                // represent the logged-in user. In a typical application, you would want
-                // to associate the spotify account with a user record in your database,
-                // and return that user instead.
+
                 return done(null, profile);
-              });
-            // User.findOrCreate({
-            //     spotifyId: profile.id
-            // }, function (err, user) {
-            //     return done(err, user);
-            // });
+            });
+
         }
     )
 );
@@ -88,7 +99,124 @@ app.use(passport.session());
 
 app.get('/', handlers.home)
 
-app.get('/auth/spotify', passport.authenticate('spotify'));
+
+app.use('/list/spotify', function (req, res, next) {
+    // res.redirect('/')
+    next()
+})
+
+async function getProfile(accessToken) {
+
+    let response = await fetch('https://api.spotify.com/v1/me', {
+        headers: {
+            Authorization: 'Bearer ' + accessToken
+        }
+    });
+
+    let data = await response.json();
+    return data;
+}
+
+
+async function getPlaylists(accessToken, userID, offset = 0, url, playlists = []) {
+    // console.log(userID)
+    // console.log(accessToken)
+
+    if (url == null) {
+        var response = await fetch(`https://api.spotify.com/v1/users/${userID}/playlists?offset=${offset}&limit=50`, {
+            headers: {
+                Authorization: 'Bearer ' + accessToken
+            }
+        })
+    } else {
+        var response = await fetch(url, {
+            headers: {
+                Authorization: 'Bearer ' + accessToken
+            }
+        })
+    }
+
+    let thisResp = await response
+    // let item = await thisResp.text() 
+    let data = await thisResp.json();
+    // playlists.push(data['items'])
+    for (const elem of data['items']) {
+        playlists.push(elem)
+        playlistSpotify.push(elem)
+    }
+
+    // console.log("Size of playlist data stuff: " + playlists.length)
+    // console.log("Type: "+ typeof data["items"])
+    // console.log("Length: "+ data["items"].length)
+
+    // if(offset = 0){
+    //     localStorage.setItem("sPlaylist", data["items"])
+    //     console.log("Type: "+ typeof localStorage.getItem("sPlaylist"))
+    // }
+    // else{
+    //     console.log("Type: "+typeof localStorage.getItem("sPlaylist"))
+    //     // localStorage.getItem("sPlaylist").push(data['items'])
+    // }
+    if (data['next'] != null) {
+        // console.log("MORE!!!!!!!!!!!!!!!!!!!!")
+        getPlaylists(accessToken, userID, offset + data['limit'], url, playlists)
+    } else {
+        localStorage.setItem("sPlaylist", JSON.stringify(playlists))
+    }
+    // console.log(data)
+    // console.log("Playlist: ", data)
+    return data;
+}
+
+app.get("/list/spotify", function (req, res) {
+    // const spotifyData = await getProfile(localStorage.getItem("sAccessToken"))
+
+    getProfile(localStorage.getItem("sAccessToken")).then(
+        result => {
+            localStorage.setItem("sID", result["id"])
+        }).
+    then((resultPlaylist) => {
+        return getPlaylists(localStorage.getItem("sAccessToken"), localStorage.getItem("sID"))
+    }).
+    then((resPlay) => {
+            let jsonPlaylist = {}
+            localStorage.setItem("sPlaylistRet", JSON.stringify(resPlay))
+            let count = 0
+            for(const elem of playlistSpotify){
+                jsonPlaylist[count] = elem
+                // console.log(elem)
+            }
+            // console.log("Json elem at 0: "+ JSON.stringify(jsonPlaylist[0]["name"]))
+            // console.log("Array at 0: " + JSON.stringify(playlistSpotify[0]))
+            // console.log()
+            // console.log(JSON.stringify(localStorage.getItem("sPlaylist")))
+
+        })
+        .then(
+
+            res.render("home", {
+                accessToken: localStorage.getItem("sAccessToken"),
+                refreshToken: localStorage.getItem("sRefreshToken"),
+                expires_in: localStorage.getItem("sExpires"),
+                profile: JSON.stringify(localStorage.getItem("sProfile")),
+                id: localStorage.getItem("sID"),
+                profileData: JSON.stringify((playlistSpotify[0])),
+                spotifyPlaylist:  encodeURIComponent(JSON.stringify(playlistSpotify))
+            })
+        ).catch(err => {
+            console.log(err);
+            res.sendStatus(501);
+        })
+
+    // res.render("home",{
+    //     accessToken: localStorage.getItem("sAccessToken"),
+    //     refreshToken: localStorage.getItem("sRefreshToken"),
+    //     expires_in: localStorage.getItem("sExpires"),
+    //     profile: JSON.stringify(localStorage.getItem("sProfile")),
+    //     profileData: JSON.stringify(spotifyData)
+    // })
+    // console.log("Done!")
+})
 
 app.get(
     '/auth/spotify/callback',
@@ -104,23 +232,27 @@ app.get(
 app.get(
     '/auth/spotify',
     passport.authenticate('spotify', {
-      scope: ['user-read-email', 'user-read-private','playlist-read-private','playlist-read-collaborative','user-top-read','user-library-read'],
-      showDialog: true,
+        // scope: ["playlist-read-private"],
+
+        scope: ['user-read-email', 'user-read-private', "playlist-read-private", 'user-library-read'],
+        showDialog: true,
     })
-  );
+);
 
-  app.get(
+app.get(
     authCallbackPath,
-    passport.authenticate('spotify', {failureRedirect: '/login'}),
+    passport.authenticate('spotify', {
+        failureRedirect: '/login'
+    }),
     function (req, res) {
-      res.redirect('/');
+        res.redirect('/');
     }
-  );
+);
 
-  app.get('/logout', function (req, res) {
+app.get('/logout', function (req, res) {
     req.logout();
     res.redirect('/');
-  });
+});
 
 
 if (require.main === module) {
@@ -133,7 +265,7 @@ if (require.main === module) {
 
 function ensureAuthenticated(req, res, next) {
     if (req.isAuthenticated()) {
-      return next();
+        return next();
     }
     res.redirect('/login');
-  }
+}
